a4b8fd27024623222c15dd4e8b5b9dcbcedcc00f
imsamuka <imsamuka@gmail.com>
2021-09-21 22:44:19 -0300

    created main()

--- CAH_TXT-Generator.py
+++ CAH_TXT-Generator.py
@@ -1,53 +1,61 @@
 import CAH_Database
 import argparse
 
+def main():
 
-# ArgumentParser ------------------
-parser = argparse.ArgumentParser(description='Rewritte a .txt file of CAH_Database to be a little prettier :D')
-parser.add_argument('--input_file',  type=str, help='the .txt file you want rewritten')
-parser.add_argument('--output_file', type=str, default='', help='the output file. (optional)')
-parser.add_argument('--language',    type=str, default='', help='language of information. Default is ENG but currently can accept "pt-br" too.')
-args = parser.parse_args()
+    # ArgumentParser -------------------------------
 
-INPUT_FILE = args.input_file
-OUTPUT_FILE = args.output_file
-LANG = args.language.lower()
-# ----------------------------------
+    parser = argparse.ArgumentParser(description='Rewritte a .txt file of CAH_Database to be a little prettier :D')
+    parser.add_argument('--input_file',  type=str, help='the .txt file you want rewritten')
+    parser.add_argument('--output_file', type=str, default='', help='the output file. (optional)')
+    parser.add_argument('--language',    type=str, default='', help='language of information. Default is ENG but currently can accept "pt-br" too.')
+    args = parser.parse_args()
 
-if LANG == 'pt-br' or LANG == 'br':
-    fmt = "-formatado"
-    winfs = " Cada linha a partir daqui vai ser considerado uma CARTA BRANCA"
-    binfs = " Cada linha a partir daqui vai ser considerado uma CARTA PRETA"
-else:
-    fmt = "-formatted"
-    winfs = " Each line from here will be considered a WHITE CARD"
-    binfs = " Each line from here will be considered a BLACK CARD"
+    INPUT_FILE = args.input_file
+    OUTPUT_FILE = args.output_file
+    LANG = args.language.lower()
 
-# ----------------------------------------------
-UseDialogBoxes = True
+    # ----------------------------------------------
 
-if UseDialogBoxes and not INPUT_FILE:
-    INPUT_FILE = CAH_Database.dialogSelectInputFile()
-
-# -------------------
-
-if not INPUT_FILE:
-    print('There is no input file!\n Insert one by adding a "--input_file" argument and then your file')
-    exit(1)
-
-if not OUTPUT_FILE:
-    # Split the input file path by '/'
-    path_list = INPUT_FILE.split("/")
-    if "." in path_list[-1]:
-        # Split the filename by '.' (extension)
-        fn = path_list[-1].split(".", 1)
-        path_list[-1] = fn[0] + fmt + "." + fn[1]
+    if LANG == 'pt-br' or LANG == 'br':
+        fmt = "-formatado"
+        winfs = " Cada linha a partir daqui vai ser considerado uma CARTA BRANCA"
+        binfs = " Cada linha a partir daqui vai ser considerado uma CARTA PRETA"
     else:
-        path_list[-1] += fmt
-    OUTPUT_FILE = "/".join(path_list)
+        fmt = "-formatted"
+        winfs = " Each line from here will be considered a WHITE CARD"
+        binfs = " Each line from here will be considered a BLACK CARD"
 
-# ----------------------------------------------
+    # ----------------------------------------------
 
-cards = CAH_Database.readTxtFile(INPUT_FILE)
-infos = (winfs, binfs)
-CAH_Database.writeTxtFile(OUTPUT_FILE, cards, infos)
+    # If no INPUT_FILE from ArgumentParser, ask in a dialog
+    if not INPUT_FILE:
+        INPUT_FILE = CAH_Database.dialogSelectInputFile()
+
+    # Still no file? error!
+    if not INPUT_FILE:
+        print('There is no input file!\n Insert one by adding a "--input_file" argument and then your file')
+        exit(1)
+
+    # Use INPUT_FILE + fmt as the default OUTPUT_FILE
+    if not OUTPUT_FILE:
+        # Split the input file path by '/'
+        path_list = INPUT_FILE.split("/")
+        if "." in path_list[-1]:
+            # Split the filename by '.' (extension)
+            fn = path_list[-1].split(".", 1)
+            path_list[-1] = fn[0] + fmt + "." + fn[1]
+        else:
+            path_list[-1] += fmt
+        OUTPUT_FILE = "/".join(path_list)
+
+    # ----------------------------------------------
+
+    cards = CAH_Database.readTxtFile(INPUT_FILE)
+    infos = (winfs, binfs)
+    CAH_Database.writeTxtFile(OUTPUT_FILE, cards, infos)
+
+
+
+if __name__ == "__main__":
+    main()
